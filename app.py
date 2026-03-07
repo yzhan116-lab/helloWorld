c14c3219fdc0045ae82e5abbc0f4ab12fda4ece6
yierzhang <yzhan116@terpmail.umd.edu>
2026-03-06 19:20:25 -0500

    Add about-css route

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def hello():
 def about():
     return render_template('about.html')
 
+@app.route('/about-css')
+def about_css():
+    return render_template('about-css.html')
+
 if __name__ == '__main__':
     app.run()
