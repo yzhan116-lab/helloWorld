e1f5ad594ba28cbc03848676365c5f8f725df6fd
yierzhang <yzhan116@terpmail.umd.edu>
2026-04-03 00:35:45 -0400

    add models py

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 db = SQLAlchemy()
 
 class Student(db.Model):
@@ -7,7 +8,7 @@ class Student(db.Model):
     student_id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(50), nullable=True)
     major_id = db.Column(db.Integer, db.ForeignKey('major.major_id'))
     birth_date = db.Column(db.DateTime, nullable=False)
     num_credits_completed = db.Column(db.Integer, nullable=False)
@@ -39,3 +40,30 @@ class Major(db.Model):
 
     def __repr__(self):
         return f"{self.major}"
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = "user"
+
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    first_name = db.Column(db.String(30))
+    last_name = db.Column(db.String(50))
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    role = db.Column(db.String(20))
+
+    def __init__(self, username, first_name, last_name, email, password, role='PUBLIC'):
+        self.username = username
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.role = role
+
+    # Function for flask_login manager to provider a user ID to know who is logged in
+    def get_id(self):
+        return(self.user_id)
+
+    def __repr__(self):
+        return f"{self.first_name} {self.last_name} ({self.username})"
